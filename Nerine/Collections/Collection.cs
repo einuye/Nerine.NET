54c58e88ce9fc767e15a9c99e81d7bca1171eaf8
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-29 09:48:39 -0500

    collections

---
+++ Nerine/Collections/Collection.cs
@@ -0,0 +1,63 @@
+﻿using Nerine.Collections.Tables;
+using System.Collections.Generic;
+using Nerine.Exceptions;
+using Nerine.IO;
+
+namespace Nerine.Collections
+{
+    public class Collection
+    {
+        public List<Table> Tables = new List<Table>();
+
+        public string Name;
+
+        public Collection(string name)
+        {
+            Name = name;
+        }
+
+        public Table AddTable(string name, List<Column> columns)
+        {
+            if (Tables.Find(x => x.Name == name) != null)
+                throw new TableExistsException("This table already exists!");
+
+            var table = new Table()
+            {
+                Name = name,
+                Columns = columns
+            };
+            Tables.Add(table);
+
+            return table;
+        }
+
+        // TODO: Find a better way to do this.
+        public byte[] ToBytes()
+        {
+            var bytes = new DynamicByteArray();
+            var data = new DynamicByteArray();
+
+            // write type
+            bytes.Write<byte>((byte) BlockType.Collection);
+
+            // write name
+            data.Write<byte>(0x1F);
+            data.Write<short>((short)Name.Length);
+            data.Write<string>(Name);
+
+            // write tables
+            foreach (var table in Tables)
+            {
+                data.Write<byte[]>(table.ToBytes());
+            }
+
+            // write length
+            bytes.Write<int>(data.Length);
+
+            // write the data
+            bytes.Write<byte[]>(data.Bytes);
+
+            return bytes.Bytes;
+        }
+    }
+}
