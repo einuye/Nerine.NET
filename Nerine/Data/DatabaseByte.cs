6d67d7f61ef3f7e5ca1b7173c0a24bf24c3a40ff
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-29 14:35:33 -0500

    bytes

---
+++ Nerine/Data/DatabaseByte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nerine.Data
+{
+    public class DatabaseByte : Structure
+    {
+        public byte Value;
+        public DatabaseByte(byte[] bytes)
+        {
+            using (var bw = new BinaryReader(new MemoryStream(bytes)))
+            {
+                Value = bw.ReadByte();
+
+                bw.Close();
+            }
+        }
+    }
+}
