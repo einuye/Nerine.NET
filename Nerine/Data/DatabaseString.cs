164056dba40fd65c4b76baec0585762bff125468
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-29 09:48:24 -0500

    strings

---
+++ Nerine/Data/DatabaseString.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Nerine.Data
+{
+    public class DatabaseString : Structure
+    {
+        public string String;
+
+        public DatabaseString(byte[] bytes)
+        {
+            Debug.WriteLine(bytes.Length);
+            using (var bw = new BinaryReader(new MemoryStream(bytes)))
+            {
+                if (bw.ReadByte() == 0x1F) // string
+                {
+                    var length = bw.ReadInt16();
+                    String = Encoding.UTF8.GetString(bw.ReadBytes(length));
+                }
+
+                bw.Close();
+            }
+        }
+    }
+}
--- Nerine/Data/Structure.cs
+++ Nerine/Data/Structure.cs
@@ -7,49 +7,16 @@ using System.Threading.Tasks;
 
 namespace Nerine.Data
 {
-    public class Structure<T>
+    public class Structure
     {
-        /*public static Structure<T> Decode<T>()
+        public static Structure Construct(StructureType type, byte[] bytes)
         {
-            if (typeof)
-        }*/
-
-        public static int SizeOf<T>()
-        {
-            Type type = typeof(T);
-
-            return Marshal.SizeOf(type);
-        }
-
-        public static T Transfer<T>(byte[] data)
-        {
-            Type type = typeof(T);
-            object d;
-
-            if (type == typeof(byte))
-                d = data[0];
-            else if (type == typeof(ushort))
-                d = BitConverter.ToUInt16(data, 0);
-            else if (type == typeof(short))
-                d = BitConverter.ToInt16(data, 0);
-            else if (type == typeof(uint))
-                d = BitConverter.ToUInt32(data, 0);
-            else if (type == typeof(int))
-                d = BitConverter.ToInt32(data, 0);
-            else if (type == typeof(ulong))
-                d = BitConverter.ToUInt64(data, 0);
-            else if (type == typeof(long))
-                d = BitConverter.ToInt64(data, 0);
-            else if (type == typeof(float))
-                d = BitConverter.ToSingle(data, 0);
-            else if (type == typeof(double))
-                d = BitConverter.ToDouble(data, 0); // oh no!!!!! double floating point values suck
-            else if (type == typeof(string))
-                d = Encoding.UTF8.GetString(data);
-            else
-                d = default(T);
-
-            return (T)d;
+            switch (type)
+            {
+                case StructureType.String:
+                    return new DatabaseString(bytes);
+            }
+            return new Structure();
         }
     }
 
