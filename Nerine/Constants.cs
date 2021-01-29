889072b6a3e4234dd5d16f180387168b94b46860
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-29 09:47:41 -0500

    constants

---
+++ Nerine/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nerine
+{
+    public static class Constants
+    {
+        public const byte SEPARATOR = 0x1D;
+    }
+}
