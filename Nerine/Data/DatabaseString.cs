5a603c0d777e9557323825c0a9d2a238ed796699
Ausakura <17104706+Ausakura@users.noreply.github.com>
2021-01-29 14:35:48 -0500

    normalize to Value

--- Nerine/Data/DatabaseString.cs
+++ Nerine/Data/DatabaseString.cs
@@ -7,17 +7,16 @@ namespace Nerine.Data
 {
     public class DatabaseString : Structure
     {
-        public string String;
+        public string Value;
 
         public DatabaseString(byte[] bytes)
         {
-            Debug.WriteLine(bytes.Length);
             using (var bw = new BinaryReader(new MemoryStream(bytes)))
             {
                 if (bw.ReadByte() == 0x1F) // string
                 {
                     var length = bw.ReadInt16();
-                    String = Encoding.UTF8.GetString(bw.ReadBytes(length));
+                    Value = Encoding.UTF8.GetString(bw.ReadBytes(length));
                 }
 
                 bw.Close();
--- Nerine/Data/Structure.cs
+++ Nerine/Data/Structure.cs
@@ -9,13 +9,41 @@ namespace Nerine.Data
 {
     public class Structure
     {
+        public object Value = null;
+
+        public static int GetSize(StructureType type)
+        {
+            switch (type)
+            {
+                case StructureType.Byte:
+                    return 1;
+            }
+
+            return 1;
+        }
+        public static Type GetType(StructureType type)
+        {
+            switch (type)
+            {
+                case StructureType.Byte:
+                    return typeof(byte);
+            }
+
+            return null;
+        }
+
         public static Structure Construct(StructureType type, byte[] bytes)
         {
             switch (type)
             {
                 case StructureType.String:
                     return new DatabaseString(bytes);
+
+                case StructureType.Byte:
+                    return new DatabaseByte(bytes);
             }
+
+            // default
             return new Structure();
         }
     }
@@ -23,5 +51,6 @@ namespace Nerine.Data
     public enum StructureType
     {
         String = 1,
+        Byte = 2,
     }
 }
